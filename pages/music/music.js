354391506bd6537545c89d3a0363638509fa090f
mengtuo <mengtuo@outlook.com>
2019-06-25 19:40:46 +0800

    实现播放圆向外扩散动画,以及顶部导航布局

--- pages/music/music.js
+++ pages/music/music.js
@@ -1,4 +1,7 @@
-import {baseURL,musicURL} from '../../apis/index'
+import {baseURL,findMusic} from '../../apis/index'
+import regeneratorRuntime from '../../utils/runtime'
+import {formatTime,getWeek,wxRequest} from '../../utils/util'
+import  * as watch from '../../utils/watch'
 // pages/music/music.js
 Page({
 
@@ -7,31 +10,32 @@ Page({
    */
   data: {
     musics: [],
-    baseURL:baseURL
+    baseURL:baseURL,
+    style: '',
+    musicTitle:'',
+    backgroundColors: ['187,255,255,0.3','255,255,240,0.3','233,150,122,0.3','191,239,255,0.3','155,48,255,0.3','193,205,205,0.3','255,255,224,0.3'],
+    musicInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var _this = this;
-    console.log(_this);
-    wx.request({
-      url: musicURL,
-      header: {
-        'Content-Type': 'application/json'
-      },
-      data:{
-        pageSize: 10,
-        pageNo:1
-      },
-      success: function(res) {
-          console.log("获取到的音乐",res);
-          _this.setData({
-            musics: res.data.data
-          })
-      }
+  onLoad: async function (options) {
+    watch.setWatcher(this);
+
+    var rs = await wxRequest(findMusic);
+    console.log("我先触发",rs);
+    /** 
+     * 设置默认的播放背景图,是获取到的数组的第一张
+     * */ 
+    var style = `background:url(${baseURL}${rs.data[0].icon}) no-repeat center;background-size:auto 100%;background-position-x:-60px;height:100%`
+    this.setData({
+      musics:rs.data,
+      musicInfo:rs.data[0],
+      style:style,
+      musicTitle:rs.data[0].name
     })
+    // this.startPlay();
   },
 
   /**
@@ -40,8 +44,46 @@ Page({
   onReady: function () {
 
   },
-  play(){
-    console.log("播放");
+  watch:{
+    musicInfo:{
+      handler(newValue) {
+        console.log("新的值",newValue);
+        var _this = this;
+        setTimeout(function(){
+          _this.startPlay();
+        },0)
+      },
+      deep:true
+    }
+  },
+  startPlay(){
+    var musicInfo = this.data.musicInfo;
+    console.log("开始播放",musicInfo);
+    if(musicInfo.name){
+      const backgroundAudioManager = wx.getBackgroundAudioManager()
+      backgroundAudioManager.title = musicInfo.name
+      backgroundAudioManager.epname = musicInfo.name
+      backgroundAudioManager.coverImgUrl = baseURL+musicInfo.icon;
+      // 设置了 src 之后会自动播放
+      backgroundAudioManager.src = baseURL+musicInfo.xm4aPath;
+
+    }
+  },
+  closePlayView(){
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  stop(){
+    wx.pauseBackgroundAudio()
+  },
+  changeMusic(ev){
+    console.log(ev);
+    var musics = this.data.musics;
+    this.setData({
+        musicTitle:musics[ev.detail.current].name,
+        musicInfo: musics[ev.detail.current]
+    })
   },
   /**
    * 生命周期函数--监听页面显示
